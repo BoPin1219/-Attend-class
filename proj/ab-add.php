f8c90bccd33dc393251c2548954fb3d938671580
Bo Pin <bopin1219@gmail.com>
2022-05-26 13:44:43 +0800

    更改

--- proj/ab-add.php
+++ proj/ab-add.php
@@ -19,37 +19,39 @@ $title = '新增通訊錄資料 - 小新的網站';
         <div class="col-md-6">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">新增基本資料</h5>
+                    <h5 class="card-title">新增資料</h5>
                     <form name="form1" onsubmit="sendData();return false;" novalidate>
                         <div class="mb-3">
-                            <label for="name" class="form-label">*姓名name</label>
+                            <label for="name" class="form-label">* name</label>
                             <input type="text" class="form-control" id="name" name="name" required>
                             <div class="form-text red"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="email" class="form-label">信箱email</label>
+                            <label for="email" class="form-label">email</label>
                             <input type="email" class="form-control" id="email" name="email">
                             <div class="form-text red"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="mobile" class="form-label">電話mobile</label>
+                            <label for="mobile" class="form-label">mobile</label>
                             <input type="text" class="form-control" id="mobile" name="mobile" pattern="09\d{8}">
                             <div class="form-text red"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="birthday" class="form-label">出生日期birthday</label>
+                            <label for="birthday" class="form-label">birthday</label>
                             <input type="date" class="form-control" id="birthday" name="birthday">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="address" class="form-label">地址address</label>
+                            <label for="address" class="form-label">address</label>
                             <textarea class="form-control" name="address" id="address" cols="30" rows="3"></textarea>
                             <div class="form-text"></div>
                         </div>
 
-                        <button type="submit" class="btn btn-primary">送出</button>
+                        <button type="submit" class="btn btn-primary">新增</button>
                     </form>
-
+                    <div id="info-bar" class="alert alert-success" role="alert" style="display:none;">
+                        資料新增成功
+                    </div>
                 </div>
             </div>
         </div>
@@ -61,6 +63,7 @@ $title = '新增通訊錄資料 - 小新的網站';
     const email_re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zAZ]{2,}))$/;
     const mobile_re = /^09\d{2}-?\d{3}-?\d{3}$/;
 
+    const info_bar = document.querySelector('#info-bar');
     const name_f = document.form1.name;
     const email_f = document.form1.email;
     const mobile_f = document.form1.mobile;
@@ -79,6 +82,7 @@ $title = '新增通訊錄資料 - 小新的網站';
             fields[i].classList.remove('red');
             fieldTexts[i].innerText = '';
         }
+        info_bar.style.display = 'none'; // 隱藏訊息列
 
         // TODO: 欄位檢查, 前端的檢查
         let isPass = true; // 預設是通過檢查的
@@ -116,7 +120,20 @@ $title = '新增通訊錄資料 - 小新的網站';
         });
         const result = await r.json();
         console.log(result);
+        info_bar.style.display = 'block'; // 顯示訊息列
+        if (result.success) {
+            info_bar.classList.remove('alert-danger');
+            info_bar.classList.add('alert-success');
+            info_bar.innerText = '新增成功';
 
+            setTimeout(() => {
+                // location.href = 'ab-list.php'; // 跳轉到列表頁
+            }, 2000);
+        } else {
+            info_bar.classList.remove('alert-success');
+            info_bar.classList.add('alert-danger');
+            info_bar.innerText = result.error || '資料無法新增';
+        }
 
     }
 </script>
